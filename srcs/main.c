ad4d51247fa5776bfca169a8851401be98434ffb
contrbas@gmail.com <K4rmU78B>
2019-03-22 12:37:49 +0200

    project is ready for development

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 12:36:29 by otimofie          #+#    #+#             */
-/*   Updated: 2019/03/22 12:36:46 by otimofie         ###   ########.fr       */
+/*   Updated: 2019/03/22 12:37:18 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,8 @@
 int		main(void)
 {
 	ft_printf("And here we go\n");
+	
+	system("leaks -q ft_select");
+	
 	return (0);
 }
