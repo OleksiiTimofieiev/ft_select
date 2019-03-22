3ab8ddd10c2da5f75de7cf4c454faa4889dc98fb
contrbas@gmail.com <K4rmU78B>
2019-03-22 12:36:51 +0200

    Makefile is ready to work

--- includes/ft_select.h
+++ includes/ft_select.h
@@ -6,13 +6,13 @@
 /*   By: otimofie <otimofie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 12:31:17 by otimofie          #+#    #+#             */
-/*   Updated: 2019/03/22 12:31:45 by otimofie         ###   ########.fr       */
+/*   Updated: 2019/03/22 12:35:15 by otimofie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_SELECT_H
-#define FT_SELECT_H
+# define FT_SELECT_H
 
-#include "../libft/libft.h"
+# include "../libft/libft.h"
 
 #endif
